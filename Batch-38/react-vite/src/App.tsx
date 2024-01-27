71c61918524161216fe25ed504e35a9a2811a4bb
nvl207 <nguyenvanloi.master@gmail.com>
2024-01-27 13:54:33 +0700

    up up

--- Batch-38/react-vite/src/App.tsx
+++ Batch-38/react-vite/src/App.tsx
@@ -75,7 +75,7 @@ function App() {
     <img src="./images/vivo.jpg" alt="" />
     <img src="./images/vivo.jpg" alt="" />
     <ButtonV2 icon = {<FaShoppingBag />} lable="Thêm vào giỏ hàng"/>
-    <ButtonV2 icon ={<FaPhone />} type= "button_dark" lable="Gọi lại tư vấn"/>
+    <ButtonV2 icon ={<FaPhone />} type= "button_dark" lable="Gọi lại đặt hàng"/>
       <h1
         style={{
           color: "blue",
